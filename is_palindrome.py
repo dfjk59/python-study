4ce7cdd45e08c21aaf408a1f57c89ad3bf4c9870
dfjk59 <dfjk59@126.com>
2017-06-11 14:57:37 +0800

    init

--- is_palindrome.py
+++
@@ -1,6 +0,0 @@
-#回数是指从左向右读和从右向左读都是一样的数，例如12321，909。请利用filter()滤掉非回数：
-def is_palindrome(n):
-    return n > 10 and str(n) == str(n)[::-1]
-
-output = filter(is_palindrome, range(1, 1000))
-print(list(output))
